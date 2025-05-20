43ad456a736b6a970f365aa7adc87e79a62e5698
paisible7 <22km127@esisalama.org>
2025-05-20 17:12:45 +0200

    COMMIT_20_MAI_2025 ==> iniitialisation

---
+++ ping/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ping', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Text('Bienvenu sur Ping\nune application pour marquer la presence dans les !'),
+      ),
+    );
+  }
+}
